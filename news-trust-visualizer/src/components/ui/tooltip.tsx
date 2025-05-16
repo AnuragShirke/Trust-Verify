77f51fe76c38f6857153fec37b61a02bf7817158
AnuragShirke <anurag.shirke@example.com>
2025-05-16 21:34:21 +0530

    Inline utility function in tooltip component for Vercel deployment

--- news-trust-visualizer/src/components/ui/tooltip.tsx
+++ news-trust-visualizer/src/components/ui/tooltip.tsx
@@ -1,7 +1,12 @@
 import * as React from "react"
 import * as TooltipPrimitive from "@radix-ui/react-tooltip"
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
-import { cn } from "@/lib/utils"
+// Inline the utility function from utils.ts
+function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
 
 const TooltipProvider = TooltipPrimitive.Provider
 
